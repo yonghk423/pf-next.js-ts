f251557558136bb0b0baddf3d647fee82f49c39a
yonghk423 <yonghk423423@gmail.com>
2022-05-01 12:38:43 +0900

    project scss

--- components/Project.tsx
+++ components/Project.tsx
@@ -8,44 +8,17 @@ export default function Project() {
             <div className={styles.camera}></div>
             <div className={styles.laptop}>
               <div className={styles.projectBox}>
-                <div className={styles.projectTitle}>추억을 담다(2021.12.01 1인 개인 프로젝트)</div>
-                <img className={styles.projectImg} 
-                src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
+                <div className={styles.projectTitleBox}>
+                  <div className={styles.projectTitle}>추억을 담다</div>
+                </div>
+                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
                 <div className={styles.projectIntroBox}>
-                  <div className={styles.projectIntro}>주요기능</div>
-                  <p className={styles.projectIntroText}>테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중. </p>
+                  <div className={styles.projectIntro}></div>
+                  <p className={styles.projectIntroText}>테스트중. 테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중.테스트중. 테스트중.테스트중.테스트중.테스트중.</p>
                 </div>
-                <div className={styles.projectSkillsBox}>
-                  <div className={styles.projectSkills}>스킬 소개</div>
-                  <p className={styles.projectSkillsText}>테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.</p>
-                </div>
+               
               </div>
-                <div className={styles.projectBox}>
-                <div className={styles.projectTitle}>추억을 담다(2021.12.01 1인 개인 프로젝트)</div>
-                <img className={styles.projectImg} 
-                src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
-                <div className={styles.projectIntroBox}>
-                  <div className={styles.projectIntro}>주요기능</div>
-                  <p className={styles.projectIntroText}>테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중. </p>
-                </div>
-                <div className={styles.projectSkillsBox}>
-                  <div className={styles.projectSkills}>스킬 소개</div>
-                  <p className={styles.projectSkillsText}>테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.</p>
-                </div>
-              </div>
-               <div className={styles.projectBox}>
-                <div className={styles.projectTitle}>추억을 담다(2021.12.01 1인 개인 프로젝트)</div>
-                <img className={styles.projectImg} 
-                src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
-                <div className={styles.projectIntroBox}>
-                  <div className={styles.projectIntro}>주요기능</div>
-                  <p className={styles.projectIntroText}>테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중. </p>
-                </div>
-                <div className={styles.projectSkillsBox}>
-                  <div className={styles.projectSkills}>스킬 소개</div>
-                  <p className={styles.projectSkillsText}>테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.테스트 중.</p>
-                </div>
-              </div>
+                
             </div>                      
         </div>
         </>
