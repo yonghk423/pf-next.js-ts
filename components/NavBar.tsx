1c947be0b0bf9c773432f10186fc62d7a35bd1ba
yonghk423 <yonghk423423@gmail.com>
2022-05-01 15:23:18 +0900

    navbar scss

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,20 +4,25 @@ import Image from 'next/image';
      return (
        <div>
          <div className={styles.background}>
-           <div className={styles.navBar}>
-             <div className={styles.navBarLogo}>yong hee</div>
+          <div className={styles.navBar}>
+             <div className={styles.navBarLogo}>yong hee&apos;Portfolio</div>
              <div className={styles.navBarMenu}>
                <div className={styles.navBarList}>About me</div>
                <div className={styles.navBarList}>Skills</div>
                <div className={styles.navBarList}>Projects</div>
-               {/* <div className={styles.navBarList}>Career</div> */}
+               {/* <div className={styles.navBarList}>Career</div> */}           
              </div>
-           </div>          
+            </div>  
+          <div className={styles.navBarProfileBox}>
+            <div className={styles.navBarProfileName}>안녕하세요, <br></br> 프론트엔드 개발자 김용희입니다.</div>
+            <div className={styles.navBarProfileIntroOne}>왜?라는 질문에 답을 얻는 과정을 지향하는 개발자가 되고자 합니다.</div>
+            <div className={styles.navBarProfileIntroTwo}>빨리 가기보다는, 지치지 않고 멀리 가는 개발자를 꿈꿉니다.</div>
+          </div>          
            <Image
              src="/backgroundImg.jpeg"
              alt=""
             layout="fill" objectFit="cover" objectPosition="center"
-          />          
+          />                  
         </div>
       </div>  
     )
--- components/Project.tsx
+++ components/Project.tsx
@@ -30,14 +30,14 @@ export default function Project() {
                   </div>
                 </div>               
               </div>
-              
+
               <div className={styles.projectBox}>
                 <div className={styles.projectTitleBox}>
                   <div className={styles.projectTitle}>추억을 담다</div>
                   <div>2021.12.01(1인 개인 프로젝트)</div>
                   <div>자세히 보기</div>
                 </div>
-                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
+                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/c2139268-56e9-46a6-a49b-71ecf20bacd1/image.png' alt=''/>
                 <div className={styles.projectIntroBox}>
                   <div className={styles.projectIntro}>즐겁게 식사도 하고 눈도 즐거웠던 장소를 사진과 함께 기록할 수 있는 사이트입니다</div>
                   <div className={styles.projectIntroTextBox}>
@@ -61,7 +61,7 @@ export default function Project() {
                   <div>2021.12.01(1인 개인 프로젝트)</div>
                   <div>자세히 보기</div>
                 </div>
-                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
+                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/764791df-8a00-4092-9d0d-5f7e2c314703/image.jpeg' alt=''/>
                 <div className={styles.projectIntroBox}>
                   <div className={styles.projectIntro}>즐겁게 식사도 하고 눈도 즐거웠던 장소를 사진과 함께 기록할 수 있는 사이트입니다</div>
                   <div className={styles.projectIntroTextBox}>
