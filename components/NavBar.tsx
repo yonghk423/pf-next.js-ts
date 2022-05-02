5ef3d00ceff928adfaa3f60e5b3e851f04bf4536
yonghk423 <yonghk423423@gmail.com>
2022-05-02 19:26:16 +0900

    main scss

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,37 +9,71 @@ import "slick-carousel/slick/slick-theme.css";
   const settings = {
       dots: true,
       infinite: true,
-      slidesToShow: 3,
+      slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      speed: 2000,
-      autoplaySpeed: 2000,
-      cssEase: "linear"
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
+      arrows: false,
     };
 
      return (
-       <div>
-         <h2>Auto Play</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
+      <div className={styles.container}>
+        <div className={styles.background}>
+          <div className={styles.navBar}>
+            <div className={styles.navBarLogo}>yonghee&apos;Portfolio</div>
+            <div className={styles.navBarMenu}>
+              <div className={styles.navBarList}>Home</div>
+              <div className={styles.navBarList}>About me</div>
+              <div className={styles.navBarList}>Skills</div>
+              <div className={styles.navBarList}>Projects</div>                         
+            </div>
+          </div>  
+          <div className={styles.navBarProfileBox}>
+            <div className={styles.navBarProfileName}>안녕하세요, <br></br> 프론트엔드 개발자 김용희입니다.</div>
+            <div className={styles.navBarProfileIntroOne}>왜?라는 질문에 답을 얻는 과정을 지향하는 <br></br> 개발자가 되고자 합니다.</div>
+            <div className={styles.navBarProfileIntroTwo}>빨리 가기보다는, 지치지 않고 멀리 가는 개발자를 꿈꿉니다.</div>
+          </div>          
+           {/* <Image
+            src="/backgroundImg.jpeg"
+            alt=""
+            layout="fill" objectFit="cover" objectPosition="center"
+          />                   */}
+        </div>
+        <div className={styles.sliderBox}>                
+          <Slider {...settings}>
+            <div className={styles.imgOneBox}>                
+              <div className={styles.imgOne}>
+                <Image
+                src="/conversation.jpeg"
+                alt=""
+                layout="fill" objectFit="cover" objectPosition="center"
+                />   
+              </div>
+              <div>소통하기 위해 노력합니다.</div>
+            </div>
+            <div className={styles.imgTwoBox}>
+              <div className={styles.imgTwo}>
+                <Image
+                src="/run.jpeg"
+                alt=""
+                layout="fill" objectFit="cover" objectPosition="center"
+                />                 
+              </div>
+              <div>꾸준함 가치를 믿습니다.</div>
+            </div>
+            <div className={styles.imgThrBox}>
+              <div className={styles.imgThr}>
+                <Image
+                src="/nevergiveup.jpeg"
+                alt=""
+                layout="fill" objectFit="cover" objectPosition="center"
+                />  
+              </div>
+              <div>쉽게 포기하지 않습니다.</div>
+            </div>          
+          </Slider> 
+        </div>  
       </div>  
     )
 }
