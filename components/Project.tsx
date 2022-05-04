e675a952bcf3bff72a015c7691af317973ff8f59
yonghk423 <yonghk423423@gmail.com>
2022-05-04 13:37:47 +0900

    project scss

--- components/Project.tsx
+++ components/Project.tsx
@@ -13,9 +13,9 @@ export default function Project() {
                   <div className={styles.projectSubTitle}>2021.12.01(1인 개인 프로젝트)</div>
                   <div className={styles.projectDetail}>자세히 보기</div>
                 </div>
-                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/764791df-8a00-4092-9d0d-5f7e2c314703/image.jpeg' alt=''/>
+                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/28be1775-cc33-4a03-8487-60de5b6b1bf6/image.png' alt=''/>
                 <div className={styles.projectIntroBox}>
-                  <div className={styles.projectIntro}>포트폴리오 웹사이트</div>
+                  <div className={styles.projectIntro}>이번 포트폴리오 웹사이트는 Next.js 프레임워크를 사용하였습니다. <br></br>  React.js 라이브러리와 비교하며 이해도를 높이고 SEO 최적화와 <br></br> SSR 방식을 도입함으로써 Next.js의 장점을 경험하고자 노력하였습니다.</div>
                   <div className={styles.projectIntroTextBox}>
                     <div className={styles.projectIntroMajor}>주요기능</div>
                     <div className={styles.projectIntroMajor}>사진 업로드 및 글 작성 기능, 즐겨찾기 추가 기능</div>
@@ -43,9 +43,9 @@ export default function Project() {
                   <div className={styles.projectSubTitle}>2021.12.01(1인 개인 프로젝트)</div>
                   <div className={styles.projectDetail}>자세히 보기</div>
                 </div>
-                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/6ad352f7-345b-412c-8452-5b45598ab674/image.png' alt=''/>
+                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/7a4dfd74-0cb3-4e21-9129-6d031bf18e42/image.png' alt=''/>
                <div className={styles.projectIntroBox}>
-                  <div className={styles.projectIntro}>즐겁게 식사도 하고 눈도 즐거웠던 장소를 사진과 함께 기록할 수 있는 사이트입니다</div>
+                  <div className={styles.projectIntro}>커피를 워낙 좋아하다 보니 카페를 자주가곤 합니다. 카페마다 감성을 담은 <br></br> 사진을 찍는것을 좋아하다보니 이것을 컨셉으로 해서 웹사이트를 만들면 좋을 것 <br></br> 같다는 생각으로 웹사이트를 개발하게 되었습니다.</div>
                   <div className={styles.projectIntroTextBox}>
                     <div className={styles.projectIntroMajor}>주요기능</div>
                     <div className={styles.projectIntroMajor}>사진 업로드 및 글 작성 기능, 즐겨찾기 추가 기능</div>
@@ -55,7 +55,7 @@ export default function Project() {
                     <div className={styles.projectIntroURL}>링크</div>
                     <div className={styles.projectIntroFront}>Frontend</div>
                     <div className={styles.projectIntroFront}>
-                      TypeScript, React.js, React Hooks API, Redux.js, Redux-Thunk, Axios, Sass(SCSS)
+                      TypeScript, React.js, React Hooks API, Redux.js, <br></br> Redux-Thunk, Axios, Sass(SCSS)
                     </div>
                     <div className={styles.projectIntroBack}>Backend</div>
                     <div className={styles.projectIntroBack}>
@@ -70,12 +70,12 @@ export default function Project() {
               <div className={styles.projectBox}>
                 <div className={styles.projectTitleBox}>
                   <div className={styles.projectTitle}>나랏말싸미 웹사이트</div>
-                  <div className={styles.projectSubTitle}>2021.11 - 2021.12((4인/4주))</div>
+                  <div className={styles.projectSubTitle}>2021.11 - 2021.12(4인/4주)</div>
                   <div className={styles.projectDetail}>자세히 보기(맡은 포지션 및 역할)</div>
                 </div>
-                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/c2139268-56e9-46a6-a49b-71ecf20bacd1/image.png' alt=''/>
+                <img className={styles.projectImg}src='https://velog.velcdn.com/images/yonghk423/post/681268c8-8980-4835-abe2-1dcafb9b4e8e/image.png' alt=''/>
                 <div className={styles.projectIntroBox}>
-                  <div className={styles.projectIntro}>나랏말싸미는 우리말을 재밌게 배워 바르게 쓸 수 있도록 관련된 퀴즈를 제공하는 사이트입니다</div>
+                  <div className={styles.projectIntro}>나랏말싸미는 우리말을 재밌게 배워 바르게 쓸 수 있도록 관련된 퀴즈를 제공하는 <br></br> 사이트입니다. 직접 퀴즈를 만들수 있으며 흥미를 유발하고, 퀴즈를 풀고 맞춤에 따라 <br></br> 마일리지를 얻어 활용할 수 있는 즐거움을 배로 증가 시킬 것 입니다.</div>
                   <div className={styles.projectIntroTextBox}>
                     <div className={styles.projectIntroMajor}>주요기능</div>
                     <div className={styles.projectIntroMajor}>퀴즈 만들기, 퀴즈 맞추기, 마일리지를 이용한 상품구매</div>
@@ -85,11 +85,11 @@ export default function Project() {
                     <div className={styles.projectIntroURL}>링크</div>
                     <div className={styles.projectIntroFront}>Frontend</div>
                     <div className={styles.projectIntroFront}>
-                      JavaScript, React.js, React Hooks API, Axios,React Router, Styled-Components
+                      JavaScript, React.js, React Hooks API, Axios <br></br> React Router, Styled-Components
                     </div>
                     <div className={styles.projectIntroBack}>Backend</div>
                     <div className={styles.projectIntroBack}>
-                      Node.js, Express.js, Nodemailer, JWT, OAuth, Sequelize, MySQL
+                      Node.js, Express.js, Nodemailer, JWT, OAuth <br></br>  Sequelize, MySQL
                     </div>
                     <div className={styles.projectIntroDeploy}>Deployment</div>
                     <div className={styles.projectIntroDeploy}>AWS</div>
