0ee45983d1fca31c66110e3604bcac268aca08de
yonghk423 <yonghk423423@gmail.com>
2022-05-05 16:18:07 +0900

    keepmemories data setting

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import KeepMemoriesRouter from './router/KeepMemories.js';
+
+const app = express();
+
+app.use('/detailPagesKeepMemories', KeepMemoriesRouter);
+
+app.use((req, res ) => {
+    res.sendStatus(404);
+})
+
+app.use((error, req, res ) => {
+    console.error(error);
+    res.sendStatus(500);
+})
+
+app.listen(8080);
