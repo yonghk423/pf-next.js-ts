67f7bd666379c4afd35983be81addf9c484b9f03
yonghk423 <yonghk423423@gmail.com>
2022-05-11 16:27:06 +0900

    skills settiong

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,14 +9,14 @@ import styles from "./Skills.module.scss"
                      <div className={styles.frontTitle}>Front-end</div>
                      <div className={styles.frontList}>
                          <div className={styles.js}>JavaScript, HTML, CSS</div>
-                         <div className={styles.ts}>TypeScript, ReactJS, Next.js</div>
+                         <div className={styles.ts}>TypeScript, React.js, Next.js</div>
                          <div className={styles.redux}>Redux.js, Redux-Thunk</div>
                          <div className={styles.scss}>Sass(SCSS), styled-components</div>
                      </div>
                  </div>
                  <div className={styles.backDeployBox}>
                      <div className={styles.backTitle}>Back-end</div>
-                     <div className={styles.backList}>Node.js, ExpressJS</div>
+                     <div className={styles.backList}>Node.js, Express.js</div>
                      <div className={styles.deployTitle}>Deployment</div>
                      <div className={styles.deployList}>Vercel, Heroku</div>
                  </div>
