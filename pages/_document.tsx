19e86521b7969473e9756d54d55d91e847614401
yonghk423 <yonghk423423@gmail.com>
2022-05-11 15:01:11 +0900

    font test

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+          <link 
+          href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital@1&display=swap" 
+          rel="stylesheet">
+          </link>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
